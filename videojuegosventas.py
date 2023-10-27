f648a799565a5620456e2d4e56252761a8f0cd3a
ItsJDiego <125430297+ItsJDiego@users.noreply.github.com>
2023-10-27 11:00:44 -0600

    Limpieza de videojuegosventas.py

--- videojuegosventas.py
+++ videojuegosventas.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
 df = pd.read_csv('dato.csv')
 display(df)
 
@@ -7,3 +9,12 @@ display(muestreo)
 
 dfna=muestreo.dropna()
 display(dfna)
+
+scaler = StandardScaler()
+
+dfna['NA_Sales'] = dfna['NA_Sales'].str.replace(',', '.')
+
+# Convertir la columna en números de punto flotante
+dfna['NA_Sales'] = dfna['NA_Sales'].astype(float)
+
+dfna['NA_Sales'] = scaler.fit_transform(dfna['NA_Sales'].values.reshape(-1, 1))
